957aa850dd4406191cbfd9a43a06a906d6404585
Nicky Pochinkov <work@nicky.pro>
2022-12-19 16:36:33 +0000

    Added pytorch changes to example.py

--- welford/example.py
+++ welford/example.py
@@ -1,14 +1,14 @@
 # %%
-import numpy as np
+import torch
 from welford import Welford
 
 # Initialize Welford object
 w = Welford()
 
-# Input data samples sequentialy
-w.add(np.array([0, 100]))
-w.add(np.array([1, 110]))
-w.add(np.array([2, 120]))
+# Itorchut data samples sequentialy
+w.add(torch.tensor([0, 100]))
+w.add(torch.tensor([1, 110]))
+w.add(torch.tensor([2, 120]))
 
 # output
 print(w.mean)  # mean --> [  1. 110.]
@@ -16,19 +16,19 @@ print(w.var_s)  # sample variance --> [1, 100]
 print(w.var_p)  # population variance --> [ 0.6666 66.66]
 
 # You can add other samples after calculating variances.
-w.add(np.array([3, 130]))
-w.add(np.array([4, 140]))
+w.add(torch.tensor([3, 130]))
+w.add(torch.tensor([4, 140]))
 
 # output with added samples
 print(w.mean)  # mean --> [  2. 120.]
 print(w.var_s)  # sample variance --> [  2.5 250. ]
 print(w.var_p)  # population variance --> [  2. 200.]
 # %%
-import numpy as np
+import torch
 from welford import Welford
 
 # Initialize Welford object with samples.
-ini = np.array([[0, 100], [1, 110], [2, 120]])
+ini = torch.tensor([[0, 100], [1, 110], [2, 120]])
 w = Welford(ini)
 
 # output
@@ -37,7 +37,7 @@ print(w.var_s)  # sample variance --> [1, 100]
 print(w.var_p)  # population variance --> [ 0.66666667 66.66666667]
 
 # add other samples through batch method
-other_samples = np.array([[3, 130], [4, 140]])
+other_samples = torch.tensor([[3, 130], [4, 140]])
 w.add_all(other_samples)
 
 # output with added samples
@@ -47,7 +47,7 @@ print(w.var_p)  # population variance --> [  2. 200.]
 
 
 # %%
-import numpy as np
+import torch
 from welford import Welford
 
 # Initialize two Welford objects
@@ -56,15 +56,15 @@ w_2 = Welford()
 
 # Each object will calculate variance of each samples in parallel.
 # On w_1
-w_1.add(np.array([0, 100]))
-w_1.add(np.array([1, 110]))
-w_1.add(np.array([2, 120]))
+w_1.add(torch.tensor([0, 100]))
+w_1.add(torch.tensor([1, 110]))
+w_1.add(torch.tensor([2, 120]))
 print(w_1.var_s)  # sample variance -->[  1. 100.]
 print(w_1.var_p)  # population variance -->[ 0.66666667 66.66666667]
 
 # On w_2
-w_2.add(np.array([3, 130]))
-w_2.add(np.array([4, 140]))
+w_2.add(torch.tensor([3, 130]))
+w_2.add(torch.tensor([4, 140]))
 print(w_2.var_s)  # sample variance -->[ 0.5 50. ]
 print(w_2.var_p)  # sample variance -->[ 0.25 25.  ]
 
