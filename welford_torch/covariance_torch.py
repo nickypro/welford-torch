ad5bf6422a21c542447813f1b419b3e8612e6838
Nicky Pochinkov <work@nicky.pro>
2023-09-15 21:33:45 +0100

    Almost feature parity (except for rollback)

--- welford_torch/covariance_torch.py
+++ welford_torch/covariance_torch.py
@@ -20,7 +20,6 @@ class OnlineCovariance:
         self.__dtype = dtype
         self.__detached = False
 
-
         if elements is None:
             self.__device = device
             self.__order = None
@@ -47,6 +46,9 @@ class OnlineCovariance:
             torch.eye(
                 self.__shape[-1], dtype=self.__dtype, device=self.__device
             ).repeat((*self.__shape[:-2], 1, 1))
+
+        if self.__detached:
+            self.detach()
 
     @property
     def count(self):
@@ -82,6 +84,20 @@ class OnlineCovariance:
         denominator = torch.sqrt(variances[..., None, :] * variances[..., :, None])
         return self.__cov / denominator
 
+    @property
+    def var_p(self):
+        """
+        tensor, The (population) variance of the added data.
+        """
+        return self.__getvars(ddof=0)
+
+    @property
+    def var_s(self):
+        """
+        tensor, The (sample) variance of the added data.
+        """
+        return self.__getvars(ddof=1)
+
     def add(self, observation):
         """
         Add the given observation to this object.
@@ -94,15 +110,31 @@ class OnlineCovariance:
             self.init_zeros(observation, device=self.__device)
 
         assert observation.shape == self.__order
+        observation = observation.to(self.__dtype).to(self.__device)
 
         self.__count += 1
         delta_at_nMin1 = observation - self.__mean
         self.__mean += delta_at_nMin1 / self.__count
         weighted_delta_at_n = (observation - self.__mean) / self.__count
 
-        D_at_n = weighted_delta_at_n.expand(self.__shape).transpose(-2, -1)
-        D = (delta_at_nMin1 * self.__identity).matmul(D_at_n.transpose(-2, -1))
+        D_at_n = self.__expand_last_dim(weighted_delta_at_n).transpose(-2, -1)
+        D = (self.__expand_last_dim(delta_at_nMin1) * self.__identity).matmul(D_at_n)
         self.__cov = self.__cov * (self.__count - 1) / self.__count + D
+
+    def add_all(self, elements):
+        """ add_all
+
+        add multiple data samples.
+
+        Args:
+            elements (array(S, D)): data samples.
+            backup_flg (boolean): if True, backup previous state for rollbacking.
+
+        """
+        # backup for rollbacking
+        elements = elements.to(dtype=self.__dtype, device=self.__device)
+        for elem in elements:
+            self.add(elem)
 
     def merge(self, other):
         """
@@ -133,8 +165,7 @@ class OnlineCovariance:
         __merged_mean = (self.mean/other.count + other.mean/self.count) * count_corr
 
         flat_mean_diff = self.__mean - other.__mean
-        repeat_shape = ( *( [1]*len(self.__shape[:-1]) ), self.__shape[-1] )
-        mean_diffs = flat_mean_diff.unsqueeze(-1).repeat(repeat_shape)
+        mean_diffs = self.__expand_last_dim(flat_mean_diff)
         __merged_cov = (self.__cov * self.count \
                            + other.__cov * other.count \
                            + mean_diffs * mean_diffs.transpose(-2, -1) * count_corr) \
@@ -145,4 +176,37 @@ class OnlineCovariance:
         self.__mean = __merged_mean
         self.__cov = __merged_cov
 
+        return self
+
+    def __getvars(self, ddof=0):
+        """
+        tensor, The variance of the added data, optionally including degrees of freedom.
+        """
+        if self.__count <= 0:
+            return None
+        min_count = ddof
+        if self.__count <= min_count:
+            return torch.full(self.__order, torch.nan, dtype=self.__dtype).to(self.__device)
+        else:
+            variances = torch.diagonal(self.__cov, dim1=-2, dim2=-1)
+            if ddof == 0:
+                return variances
+            return variances *  ( self.__count/(self.__count-ddof) )
+
+    def __expand_last_dim(self, tensor):
+        """
+        Expand the last dimension of the given tensor.
+
+        Example: tensor [2, 3, 4] (__order) -> expanded_tensor [2, 3, 4, 4] (__shape)
+        """
+        shape = tensor.shape # __order
+        repeat_shape = ( *( [1]*len(shape) ), shape[-1] ) # 1, ..., 1, n
+        return tensor.unsqueeze(-1).repeat(repeat_shape)
+
+    def detach(self):
+        self.__detached = True
+        if not (self.__shape is None):
+            self.__mean = self.__mean.detach()
+            self.__cov  = self.__cov.detach()
+            self.__identity = self.__identity.detach()
         return self
